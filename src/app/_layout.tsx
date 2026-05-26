28570d6ac37774d041a92140553ff080e75e341a
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2026-05-26 17:56:43 +0800

    feat: integrate custom toast notifications into layout; configure toast position and appearance for improved user feedback

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,22 @@
 // src/app/(web)/_layout.tsx
+import { toastConfig } from "@/components/common/CustomToast/toastConfig";
 import "@/global.css";
-import { Slot, usePathname, Redirect } from "expo-router";
+import { Slot } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
 
 
- 
-
   return (
     <SafeAreaView style={{ flex: 1, flexDirection: "row" }}>
-    
+      <Toast
+        config={toastConfig}
+        position="top"
+        topOffset={500} // ✅ balanced center
+      />
       <Slot />
-    
+
     </SafeAreaView>
   );
 }
